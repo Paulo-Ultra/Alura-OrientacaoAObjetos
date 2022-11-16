89930bc393cb48ba61c7e919af482d470a39e332
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2022-11-16 15:40:52 -0300

    Design Patterns - Estruturais e Comportamentais

---
+++ Orientação a Objetos Completo/Design Patterns Java/loja/src/main/java/br/com/alura/loja/TesteImpostos.java
@@ -0,0 +1,15 @@
+package br.com.alura.loja;
+
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.imposto.ISS;
+import br.com.alura.loja.orcamento.Orcamento;
+
+public class TesteImpostos {
+    public static void main(String[] args) {
+
+        Orcamento orcamento = new Orcamento();
+        CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
+        System.out.println(calculadora.calcular(orcamento, new ICMS(new ISS(null))));
+    }
+}
---
+++ Orientação a Objetos Completo/Design Patterns Java/loja/src/main/java/br/com/alura/loja/desconto/CalculadoraDeDescontos.java
@@ -0,0 +1,15 @@
+package br.com.alura.loja.desconto;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class CalculadoraDeDescontos {
+
+    public BigDecimal calcular(Orcamento orcamento){
+        Desconto cadeiaDeDescontos = new DescontoParaOrcamentoComMaisDeCincoItens(
+                new DescontoParaOrcamentoMaiorQueQuinhentos(
+                    new SemDesconto()));
+        return cadeiaDeDescontos.calcular(orcamento);
+    }
+}
